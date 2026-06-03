cfe0adda2113f66f0ff6fb6732e5b6952e2c3d8d
Mathias <mathias@skarving.com>
2026-06-03 13:45:17 +0200

    Add JavaDoc for Subscriber interface

--- Common/src/main/java/dk/sdu/cbse/common/ecs/Subscriber.java
+++ Common/src/main/java/dk/sdu/cbse/common/ecs/Subscriber.java
@@ -1,5 +1,14 @@
 package dk.sdu.cbse.common.ecs;
 
+/**
+ * Interface for event subscribers
+ * @param <T> Event type
+ */
 public interface Subscriber<T extends EventType> {
-    public void onEvent(T event);
+
+    /**
+     * Called when an event is published
+     * @param event the Event Type to be handled
+     */
+    void onEvent(T event);
 }
